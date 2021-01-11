b3049d16a84e4fd526454d5c5d5b7357d8d13e1f
hseob04 <hseob04@naver.com>
2021-01-11 12:49:27 +0900

    Create login.php

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <div align=center>
+    <span>로그인</span>
+    
+    <form method='get' action='login_action.php'>
+        <p>ID:<input name="id" type="text"></p>
+        <p>PW:<input name="pw" type="password"></p>
+        <input type="submit" value="로그인">
+    </form>
+    </br>
+    <button id="join" ></button>
+</body>
+</html>
