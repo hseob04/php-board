5f6db1bdcfd6e601f909347006b5a816332f09dc
hseob04 <hseob04@naver.com>
2021-01-12 10:47:04 +0900

    Update write_action.php

--- write_action.php
+++ write_action.php
@@ -1,27 +1,30 @@
 <?php
-
-    $connect = mysqli_connect("localhost","root","","board") or die("fail");
-    
-    $id = $_GET['name'];
-    $pw = $_GET['pw'];
-    $title = $_GET['title'];
-    $connect = $_GET['content'];
-    $date = date('Y-m-d H:i:s');
-
-    $url = './index.php';
-
-    $query = "insert into board (number, title, content, date, hit, id, password) values(null,'$title','$connect','$date',0,'$id','$pw')";
-
-    $result = $connect->query($query);
-    if($result){
-        ?><script>
-            alert("<?php echo $URL?>");
-        </script>
-        <?php
-    }
-    else{
-        echo "FAIL";
-    }
-    mysqli_close($connect);
-
+                $connect = mysqli_connect("localhost", "root", "", "board") or die("fail");
+                
+                $id = $_GET['name'];                      //Writer
+                $pw = $_GET['pw'];                        //Password
+                $title = $_GET['title'];                  //Title
+                $content = $_GET['content'];              //Content
+                $date = date('Y-m-d H:i:s');            //Date
+ 
+                $URL = './index.php';                   //return URL
+ 
+ 
+                $query = "insert into board (number,title, content, date, hit, id, password) 
+                        values(null,'$title', '$content', '$date',0, '$id', '$pw')";
+ 
+ 
+                $result = $connect->query($query);
+                if($result){
+?>                  <script>
+                        alert("<?php echo "글이 등록되었습니다."?>");
+                        location.replace("<?php echo $URL?>");
+                    </script>
+<?php
+                }
+                else{
+                        echo "FAIL";
+                }
+ 
+                mysqli_close($connect);
 ?>
